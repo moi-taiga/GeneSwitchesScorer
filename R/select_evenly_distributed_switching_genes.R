fe6891bd03538a36160bc3bc6cb264a56fed1212
moi-taiga <moi.t.nicholas@gmail.com>
2023-04-25 15:17:20 +0100

    Initial commit

---
+++ R/select_evenly_distributed_switching_genes.R
@@ -0,0 +1,50 @@
+#' @title Select Evenly Distributed Switching Genes
+#'
+#' @param sg_allgenes
+#' @param min_time_spacing
+#'
+#' @return
+#' @export
+#'
+#' @examples
+select_evenly_distributed_switching_genes <- function(sg_allgenes, min_time_spacing){
+
+  ## Sort sg_allgenes by pseudoR2s
+  sg_allgenes <- sg_allgenes[order(-sg_allgenes$pseudoR2s),]
+
+  # Initialize the time value of the previously selected gene for both "up"'s and "down"'s
+  ups <- sg_allgenes[sg_allgenes$direction == "up", ]
+  prev_ups_times <- ups$switch_at_timeidx[1]
+  downs <- sg_allgenes[sg_allgenes$direction == "down", ]
+  prev_downs_times <- downs$switch_at_timeidx[1]
+
+  # Initialize the subsetted matrix with the first "up" and "down" gene
+  subsetted_matrix <- downs[1, ]
+  subsetted_matrix <- rbind(subsetted_matrix, ups[1, ])
+
+  # Loop over the remaining "up" genes and add them to the subsetted matrix if they meet the criteria
+  for (i in 2:nrow(ups)) {
+    # Check if the time value of the current gene is spaced by at least min_time_spacing from all previously selected genes
+    if (all(abs(ups$switch_at_timeidx[i] - prev_ups_times) >= min_time_spacing)) {
+      # Add the current gene to the subsetted matrix
+      subsetted_matrix <- rbind(subsetted_matrix, ups[i, ])
+      # Update the previous time values
+      prev_ups_times <- c(prev_ups_times, ups$switch_at_timeidx[i])
+    }
+  }
+
+  # Loop over the remaining "down" genes and add them to the subsetted matrix if they meet the criteria
+  for (i in 2:nrow(downs)) {
+    # Check if the time value of the current gene is spaced by at least min_time_spacing from all previously selected genes
+    if (all(abs(downs$switch_at_timeidx[i] - prev_downs_times) >= min_time_spacing)) {
+      # Add the current gene to the subsetted matrix
+      subsetted_matrix <- rbind(subsetted_matrix, downs[i, ])
+      # Update the previous time values
+      prev_downs_times <- c(prev_downs_times, downs$switch_at_timeidx[i])
+    }
+  }
+
+  # return the subsetted matrix
+  gs_scorer_genes <- subsetted_matrix
+  return(gs_scorer_genes)
+}
