d670dcd6a8d3d34dd31bb1762cf3bc3d4f219b67
moi-taiga <moi.t.nicholas@gmail.com>
2023-04-26 15:26:27 +0100

    Initial commit

--- R/create_racing_lines.R
+++ R/create_racing_lines.R
@@ -1,16 +1,18 @@
-
-
-#' Identify the "racing lines"
+#' @title Identify the "racing lines"
 #'
-#' @param reduced_binary_counts_matrix
-#' @param gs_scorer_genes
+#' @description
+#' Produces an estimate for the position on trajectory of each gene in each cell of a sample.
+#'     This can be later aggregated to estimate the position of the sample along the trajectory.
+#'
+#' @param reduced_binary_counts_matrix a matrix of your samples binary gene expression.
+#' @param gs_scorer_genes Switching genes which are evenly distributed through pseudotime
 #'
 #' @return
 #' @export
 #'
-#' @examples
+#' @examples create_racing_lines(reduced_binary_counts_matrix,gs_scorer_genes)
 #'
-create_racing_lines<-function(reduced_binary_counts_matrix,gs_scorer_genes) {
+create_racing_lines <- function(reduced_binary_counts_matrix,gs_scorer_genes) {
   all_patients_cells_scored<-list()
   number_of_cells<-dim(reduced_binary_counts_matrix)[2]
   number_of_switching_genes<-dim(gs_scorer_genes) [1]
--- R/filter_gene_expression_for_switching_genes.R
+++ R/filter_gene_expression_for_switching_genes.R
@@ -4,14 +4,14 @@
 #' Create a reduced binary expression matrix for only the selected switching genes,
 #'     binary_counts_matrix is from the Patient DATA and gs_scorer_genes is from Atlas Data.
 #' @param binary_counts_matrix
-#' @param gs_scorer_genes
+#' @param gs_scorer_genes Switching genes which are evenly distributed through pseudotime..
 #'
 #' @return
 #' @export
 #'
 #' @examples
 #'
-filter_gene_expression_for_switching_genes<-function(binary_counts_matrix, gs_scorer_genes) {
+filter_gene_expression_for_switching_genes <- function(binary_counts_matrix, gs_scorer_genes) {
   indices_of_switching_genes<-which(rownames(binary_counts_matrix) %in% gs_scorer_genes[,1])
   reduced_binary_counts_matrix<- binary_counts_matrix[indices_of_switching_genes,]
   gs_scorer_genes_to_keep<-which(gs_scorer_genes[,1] %in% rownames(reduced_binary_counts_matrix))
