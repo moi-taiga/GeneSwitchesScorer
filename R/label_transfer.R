b684f8f9e542838f99e73b6a50a6000c7e670be9
moi-taiga <moi.t.nicholas@gmail.com>
2023-05-17 15:40:48 +0100

    label_transfer v1.0

--- R/label_transfer.R
+++ R/label_transfer.R
@@ -1,27 +1,30 @@
 
-#' Title
+#' Label Transfer
+#' To make sure that your labels in your patient data match that used in the atlas.
 #'
-#' @param arg1
-#' @param arg2
+#' @import Seurat
+#' @param query The Query object. e.g. the patient data
+#' @param reference The reference Seurat object. e.g the atlas data
 #'
-#' @return
+#' @return The Query object with new predicted cell types.
 #' @export
 #'
 #' @examples
-label_transfer <- function(arg1, arg2) {
+label_transfer <- function(query, reference) {
   ## Find transfer anchors
-  pre_anchors <- FindTransferAnchors(
-    reference = atlas.seu,
-    query = pre.seu,
+  anchors <- FindTransferAnchors(
+    reference = reference,
+    query = query,
     normalization.method = "SCT",
     reference.reduction = "pca",
     dims = 1:20
   )
+
   ## Map Query
-  pre.seu <- MapQuery(
-    anchorset = pre_anchors,
-    query = pre.seu,
-    reference = atlas.seu,
+  query <- MapQuery(
+    anchorset = anchors,
+    query = query,
+    reference = reference,
     refdata = list(
       cell_type = "lv1_annot"
     ),
@@ -29,14 +32,28 @@ label_transfer <- function(arg1, arg2) {
   )
 
   ## Remove cells from the patient data which don't have a confident cell_type prediction.
-  pre.seu <- subset(pre.seu, subset = predicted.cell_type.score > 0.65)
+  query <- subset(query, subset = predicted.cell_type.score > 0.65)
 
   ## Process the patient data using Seurat
-  #SCTransform again here?
-  pre.seu <- RunPCA(pre.seu)
-  pre.seu <- FindNeighbors(pre.seu)
-  pre.seu <- FindClusters(pre.seu)
-  pre.seu <- RunUMAP(pre.seu, dims = 1:30, n_neighbors = 20, min_dist = 0.3)
+
+  # I'm not sure if SCTransform would be appropriate here.
+  # query <- SCTransform(query)
+
+  # Run PCA on the patient data
+  query <- RunPCA(query)
+
+  # Find nearest neighbors in the patient data
+  query <- FindNeighbors(query)
+
+  # Cluster the patient data
+  query <- FindClusters(query)
+
+  # Run UMAP on the patient data
+  query <- RunUMAP(query, dims = 1:30, n_neighbors = 20, min_dist = 0.3)
+
+  # Return the processed patient data
+  return(query)
 }
+
 
 
