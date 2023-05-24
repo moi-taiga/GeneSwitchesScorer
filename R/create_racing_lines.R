9d4d338f21094ceb658e9ef35c560ceba42fd038
moi-taiga <moi.t.nicholas@gmail.com>
2023-05-24 16:15:41 +0100

    massive optimization

--- R/create_racing_lines.R
+++ R/create_racing_lines.R
@@ -12,48 +12,40 @@
 #' @export
 #'
 create_racing_lines <- function(reduced_binary_counts_matrix,gs_scorer_genes) {
-  all_patients_cells_scored<-list()
-  number_of_cells<-dim(reduced_binary_counts_matrix)[2]
-  number_of_switching_genes<-dim(gs_scorer_genes) [1]
-  #for each patient cell C
-  for (c in 1:number_of_cells){
-    #print(paste(c,"out of", number_of_cells, "cells"))
-    racing_mat<-matrix(0,nrow = number_of_switching_genes, ncol = 100)
+  number_of_cells <- dim(reduced_binary_counts_matrix)[2]
+  number_of_switching_genes <- dim(gs_scorer_genes)[1]
 
-    #for each switching gene G
-    for (g in 1:number_of_switching_genes){
-      #print(paste(g,"out of", number_of_switching_genes, "switching genes"))
-      #find out the switch time Gt, and direction Gd
-      #changed to 4, depends if data included geneID as column 1 or just rownames
-      switching_time <- as.numeric(gs_scorer_genes[g,12])
-      switching_direction <- gs_scorer_genes[g,10]
-      #find out if its expressed Ct
-      is_expressed <- reduced_binary_counts_matrix[g,c]
-      #If Ct = TRUE
-      if(is_expressed == 1){
-        #If Gd = UP
-        if(switching_direction == "up"){
-          # [Gt:100] = 1
-          racing_mat[g,switching_time:100]<-1
-        }else{
-          #If Gd = DOWN
-          # [0:Gt] = 1
-          racing_mat[g,0:switching_time]<-1
-        }
-        #If Ct = FALSE
-      }else{
-        if(switching_direction == "up"){
-          #If Gd = UP
-          #[0:Gt] = 1
-          racing_mat[g,0:switching_time]<-1
-        }else{
-          #If Gd = DOWN
-          # [Gt:100] = 1
-          racing_mat[g,switching_time:100]<-1
-        }
-      }
+  all_patients_cells_scored <- vector("list", number_of_cells)
+
+  switching_time <- as.numeric(gs_scorer_genes[, 12])
+  switching_direction <- gs_scorer_genes[, 10]
+
+  for (c in 1:number_of_cells) {
+   racing_mat <- matrix(0, nrow = number_of_switching_genes, ncol = 100)
+    binarized_gene_expression <- reduced_binary_counts_matrix[, c]
+
+    up_indices <- which(binarized_gene_expression == 1 & switching_direction == "up")
+    down_indices <- which(binarized_gene_expression == 1 & switching_direction == "down")
+    not_up_indices <- which(binarized_gene_expression == 0 & switching_direction == "up")
+    not_down_indices <- which(binarized_gene_expression == 0 & switching_direction == "down")
+
+    if (length(up_indices) > 0) {
+      racing_mat[up_indices, switching_time[up_indices]:100] <- 1
     }
-    all_patients_cells_scored[[c]]<-racing_mat
+
+    if (length(down_indices) > 0) {
+      racing_mat[down_indices, 1:switching_time[down_indices]] <- 1
+    }
+
+    if (length(not_up_indices) > 0) {
+      racing_mat[not_up_indices, 1:switching_time[not_up_indices]] <- 1
+    }
+
+    if (length(not_down_indices) > 0) {
+      racing_mat[not_down_indices, switching_time[not_down_indices]:100] <- 1
+    }
+
+    all_patients_cells_scored[[c]] <- racing_mat
   }
   return(all_patients_cells_scored)
 }
