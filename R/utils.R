2177b0ea3835d1a25f6cc0d62070c7240793ccca
moi-taiga <moi.t.nicholas@gmail.com>
2023-07-06 13:48:39 +0100

    Increased verbossity.

--- R/utils.R
+++ R/utils.R
@@ -10,6 +10,15 @@
 #' @export
 print.GSS_OBJECT <- function(x) {
   cat("This is an object of class 'GSS_OBJECT'\n")
-  cat("It contains", length(x)," objects:\n")
-  cat(paste(names(x), collapse = "\n"))
+  cat("It contains", length(x), "elements:\n\n")
+
+  for (e in seq_along(x)) {
+    if (is.list(x[[e]])) {
+      cat("Element", e, ":", names(x[e]), "\n")
+      cat("A List of", length(x[[e]]),  "matrices\n\n")
+    } else if (is.matrix(x[[e]])) {
+      cat("Element", e, ":", names(x[e]), "\n")
+      cat("A Matrix with dimentions of", paste(dim(x[[e]]), collapse = " x "), "\n\n")
+    }
+  }
 }
