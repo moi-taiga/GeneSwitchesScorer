3eaeb361019b8a6fc10c7f26f6d18b3096308c54
moi-taiga <moi.t.nicholas@gmail.com>
2023-07-07 14:34:05 +0100

    "Works", ouput need investigating.

--- R/accuracy_test.R
+++ R/accuracy_test.R
@@ -1,20 +1,36 @@
-#' Title
+#' Title Accuracy Tester
 #'
 #' @description
-#' Use this with "sample" cells taken from the reference. It will check how close the predicted position of the cells is to the real positon.
+#' Use this with "sample" cells taken from the reference. It will check how close the predicted position of the cells is to the real position.
 #'
-#' @param fib_flat flattened sample data
-#' @param gss_genes
+#' @param gss_obj A "GSS_OBJECT" as outputted by create_racing_lines
+#' @param reference.sce The SCE object which has had slingshot run on it already.
 #'
-#' @return A score for how accurate the reults are.
+#' @return A score for how accurate the results are as a data frame.
 #' @export
-score_gss_accuracy <- function(gss_obj) {
+score_gss_accuracy <- function(gss_obj, reference.sce) {
 
-  cells_names <- rownames(gss_obj$cells_flat)
+  # Create a data frame to store the accuracy results
+  accuracy <- data.frame(
+    cell_names = colnames(reference.sce),                                         # Cell names from reference.sce
+    true_position_of_cells_pseudotime = reference.sce@colData$slingPseudotime_1,  # True pseudotime values from reference.sce
+    true_position_of_cells_timeIDX = NA,                                          # Placeholder for true time indices
+    predicted_position_of_cells_timeIDX = NA,                                     # Placeholder for predicted time indices
+    accuracy = NA                                                                 # Placeholder for accuracy values
+  )
 
+  # Calculate the time step based on the pseudotime range
+  steptime <- (max(reference.sce@colData$slingPseudotime_1) - min(reference.sce@colData$slingPseudotime_1)) / 100
 
-  predicted_position_of_cells_timeIDX <- apply(gss_obj$cells_flat, 1, which.max)
+  # Calculate the true time indices based on the pseudotime values
+  accuracy$true_position_of_cells_timeIDX <- round((reference.sce@colData$slingPseudotime_1 - min(reference.sce@colData$slingPseudotime_1)) / steptime)
 
-  #this needs converting to IDX
-  true_position_of_cells_pseudotime <- reference.sce@colData$slingPseudotime_1
-  }
+  # Predict the time indices for the cells using the gss_obj
+  # Note: This assumes that the names of cells in gss_obj$cells_flat match the cell names in reference.sce
+  accuracy$predicted_position_of_cells_timeIDX[match(names(apply(gss_obj$cells_flat, 1, which.max)), accuracy$cell_names)] <- apply(gss_obj$cells_flat, 1, which.max)
+
+  # Calculate the accuracy as the absolute difference between the true and predicted time indices
+  accuracy$accuracy <- abs(accuracy$true_position_of_cells_timeIDX - accuracy$predicted_position_of_cells_timeIDX)
+
+  return(accuracy)
+}
