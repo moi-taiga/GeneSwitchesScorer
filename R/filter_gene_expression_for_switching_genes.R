dccc067ae6915a6413bc3d95411b1e0d187164ba
moi-taiga <moi.t.nicholas@gmail.com>
2023-07-11 12:10:33 +0100

    Ouput is always a matrix, it copes with one-cell inputs. removed old code.

--- R/filter_gene_expression_for_switching_genes.R
+++ R/filter_gene_expression_for_switching_genes.R
@@ -14,12 +14,6 @@
 
 filter_gene_expression_for_switching_genes <- function(binary_counts_matrix, gss_genes) {
   indices_of_switching_genes   <- which(rownames(binary_counts_matrix) %in% gss_genes[,1])
-  reduced_binary_counts_matrix <- binary_counts_matrix[indices_of_switching_genes,]
-  # gs_scorer_genes_to_keep<-which(gs_scorer_genes[,1] %in% rownames(reduced_binary_counts_matrix))
-  # gs_scorer_genes<- gs_scorer_genes[gs_scorer_genes_to_keep,]
-  # #print(gs_scorer_genes_to_keep)
-  # returnlist<-list(reduced_binary_counts_matrix, gs_scorer_genes)
-  # return(returnlist)
+  reduced_binary_counts_matrix <- binary_counts_matrix[indices_of_switching_genes, ,drop = FALSE]
   return(reduced_binary_counts_matrix)
-  #im new!
 }
